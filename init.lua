7bd190685f3f347e76b01c65e27ae577bc633636
ferrymehdi <mehdiferry996@gmail.com>
2025-08-08 18:15:53 +0100

    feat(feather): Initial commit

--- init.lua
+++ init.lua
@@ -1,20 +1,184 @@
-----------------------------------------------------------
---                                                      --
---      ultra minimalistic and powerful nvim config     --
---                                                      --
-----------------------------------------------------------
+---@diagnostic disable: undefined-global
 
---  leader key
+-- {{ OPTIONS:
+vim.opt.mouse = "i"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.g.have_nerd_font = false
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "  ", trail = "·", nbsp = "␣" }
+vim.opt.scrolloff = 10
+vim.opt.laststatus = 3
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.cache/nvim/undodir"
+vim.opt.undofile = true
+vim.env.term = "xterm-256color"
+vim.opt.signcolumn = "yes"
+vim.opt.path:append("**")
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 200
+vim.opt.winborder = "rounded"
+--}}
+
+--{{ PLUGINS:
+local path_package = vim.fn.stdpath("data") .. "/site/"
+local mini_path = path_package .. "pack/deps/start/mini.nvim"
+if not vim.loop.fs_stat(mini_path) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--depth=1",
+		"https://github.com/echasnovski/mini.nvim",
+		mini_path,
+	})
+	vim.cmd("packadd mini.nvim | helptags all")
+end
+require("mini.deps").setup({ path = { package = path_package } })
+
+-- instalation
+MiniDeps.add({ source = "vague2k/vague.nvim", as = "vague" })
+MiniDeps.add("nvim-treesitter/nvim-treesitter")
+MiniDeps.add("folke/snacks.nvim")
+MiniDeps.add("nvim-lua/plenary.nvim") -- required by neogit
+MiniDeps.add({ source = "neogitorg/neogit", checkout = "028e9ee" })
+MiniDeps.add("mbbill/undotree")
+
+-- configutation
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+require("snacks").setup({ picker = { enabled = true } })
+require("neogit").setup()
+--}}
+
+--{{ THEMING:
+vim.cmd("colorscheme vague")
+vim.cmd("hi statusline guibg=NONE")
+vim.cmd("highlight Pmenu guibg=#1e1e2e guifg=#cdd6f4")
+vim.cmd("highlight NormalFloat guibg=#181825 guifg=#cdd6f4")
+--}}
+
+--{{ LSP:
+vim.diagnostic.config({ virtual_text = true })
+vim.lsp.enable("lua_ls")
+vim.lsp.enable("gopls")
+vim.lsp.enable("clangd")
+vim.lsp.enable("rust_analyzer")
+vim.lsp.enable("ts_ls")
+--}}
+
+--{[ AUTOCOMMANDS:
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking text",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { buffer = args.buf, desc = "Go to definition" })
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = args.buf, desc = "Go to definition" })
+		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = args.buf, desc = "Go to declaration" })
+		vim.keymap.set("n", "gF", function()
+			vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+		end, { buffer = args.buf, desc = "Format buffer" })
+
+		if client:supports_method("textDocument/completion") then
+			vim.opt.completeopt = { "menu", "menuone", "noselect", "fuzzy", "popup" }
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+			vim.keymap.set("i", "<C-Space>", vim.lsp.completion.get)
+
+			vim.api.nvim_create_autocmd("InsertCharPre", {
+				callback = function()
+					local char = vim.v.char
+					if not char:match("%s") then
+						vim.defer_fn(function()
+							vim.lsp.completion.get()
+						end, 0)
+					end
+				end,
+			})
+		end
+	end,
+})
+--}}
+
+--{{ KEYMAPS:
 vim.g.mapleader = " "
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
--- load the plugins
-require("plugins.init")
+local function k(mode, key, action, desc, expr)
+	vim.keymap.set(mode, key, action, { desc = desc, expr = expr or false })
+end
 
--- load the options
-require("settings.options")
--- load the global funcs
-require("settings.globals")
--- load Autocommands
-require("settings.autoCommands")
--- load the keybinds
-require("settings.keybinds")
+k("", "<leader>y", '"+y', "Yank to System Clipboard")
+k("", "<leader>p", '"+p', "Paste from System Clipboard")
+k("", "yu", 'gg0vG$"+y', "Yank Buffer to System Clipboard")
+
+k("n", "<Esc>", "<cmd>nohlsearch<CR>", "Remove Search Highlighting")
+
+k("n", "-", "<cmd>Explore<CR>", "Open Netrw")
+
+k("n", "<C-d>", "<C-d>zz", "Page down")
+k("n", "<C-u>", "<C-u>zz", "Page up")
+
+k("n", "<C-h>", "<C-w><C-h>", "Focus Left")
+k("n", "<C-l>", "<C-w><C-l>", "Focus Right")
+k("n", "<C-j>", "<C-w><C-j>", "Focus Down")
+k("n", "<C-k>", "<C-w><C-k>", "Focus Up")
+
+k("n", "<S-u>", "<C-r>", "Redo")
+
+k("n", "[q", "<cmd>cprev<CR>", "Go to Previous in Quickfix List")
+k("n", "]q", "<cmd>cnext<CR>", "Go to Next in Quickfix List")
+
+k("n", "[b", "<cmd>bp<CR>", "Buffer Previous")
+k("n", "]b", "<cmd>bn<CR>", "Buffer Next")
+k("n", "<BS>", function()
+	local alt = vim.fn.bufnr("#")
+	if vim.fn.buflisted(alt) == 1 and vim.bo[alt].filetype ~= "netrw" then
+		vim.cmd("b#")
+	else
+		vim.cmd("bn")
+	end
+end, "Buffer Recent")
+
+k("n", "[t", "<cmd>tabprev<CR>", "Go to Previous Tab")
+k("n", "]t", "<cmd>tabnext<CR>", "Go to Next Tab")
+
+k("n", "<leader><leader>", ":find ", "Find files")
+
+k("n", "<leader>ff", function()
+	require("snacks").picker.files({ layout = "select" })
+end, "Find file")
+k("n", "<leader>fb", require("snacks").picker.buffers, "Find buffer")
+k("n", "<leader>fg", require("snacks").picker.grep, "Find by grep")
+k("n", "<leader>fp", require("snacks").picker.projects, "Find project")
+k("n", "<leader>fd", require("snacks").picker.diagnostics, "Find diagnostics")
+
+k("n", "<leader>tg", "<cmd>Neogit<CR>", "Toggle neogit")
+k("n", "<leader>tu", "<cmd>UndotreeToggle<CR><cmd>UndotreeFocus<CR>", "Toggle undotree")
+
+k("n", "<leader>we", "<cmd>e!<CR>", "Reloads current file from disk")
+
+k("i", "<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]], "Go down in completion menu", true)
+k("i", "<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]], "Go up in completion menu", true)
+k("i", "<C-j>", [[pumvisible() ? "\<C-n>" : "\<C-j>"]], "Go down in completion menu", true)
+k("i", "<C-k>", [[pumvisible() ? "\<C-p>" : "\<C-k>"]], "Go up in completion menu", true)
+k("i", "<CR>", [[pumvisible() ? "\<C-y>" : "\<CR>"]], "Accept selected item in completion menu", true)
+
+k("v", "J", ":m '>+1<CR>gv=gv", "Move Selected Text Down")
+k("v", "K", ":m '<-2<CR>gv=gv", "Move Selected Text Up")
+--}}
